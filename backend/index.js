ed12378a34cf5b36f0bd5e205a0b9279bdb0f541
ayat <almomanii.ayat@gmail.com>
2024-05-30 15:54:00 +0300

    main

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const connection = require("./models/db");
-const path = require("path");
-const bodyParser = require("body-parser");
+
 const app = express();
 app.use(express.json());
 const cors = require("cors");
