6321eadf1174a05d7151d3251d3b15f56f01ef56
ayat <almomanii.ayat@gmail.com>
2024-06-04 18:15:35 +0300

    main

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,12 +1,9 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
+const connection= new Pool({
+  connectionString: process.env.POSTGRES_URL,
+})
 
 connection.connect((err) => {
   if (err) {
